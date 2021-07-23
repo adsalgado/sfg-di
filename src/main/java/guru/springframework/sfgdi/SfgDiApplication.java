8f40bf6ea94a2af9c2ad899fd5f4de0115d90a40
Adrian Salgado <asalgado@santander.com.mx>
2021-07-23 10:42:21 -0500

    Version inicial

---
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgdi.controllers.MyController;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String gretting = myController.sayHello();
+		System.out.println(gretting);
+	}
+
+}
